a8183f2445762a36e32fd8e3eabe3a18d5fa7c31
markadams88 <mark.j.adams@live.co.uk>
2026-09-23 20:19:24 +0100

    Week 3 question pack: percentages
    Ten You dos with their extensions and the twelve, all real exam questions from the AUMS Higher percentages booklets. Every answer re-derived in verify/week03_percentages.py.

--- packs/index.js
+++ packs/index.js
@@ -1,5 +1,5 @@
 /* List every week's pack here. Add a line when a new week is built. */
 (function(){
-  var packs = ['week01', 'week02'];
+  var packs = ['week01', 'week02', 'week03'];
   packs.forEach(function(p){ document.write('<script src="packs/'+p+'.js"><\/script>'); });
 })();
